8479e83dbc89bed9a50c2f252cab74d65d0a63b4
Parth Shrivastava <shrivastavaparth03@gmail.com>
2025-03-28 23:07:55 +0530

    Revuilding the app

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,14 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/", // Add this line
+  build: {
+    assetsDir: "assets", // Add this section
+    manifest: true,
+    rollupOptions: {
+      input: "./index.html",
+    },
+  },
 });
