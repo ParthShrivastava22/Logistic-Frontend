70eaf0e98833dcc6aafb401dda602d4f873ec974
Parth Shrivastava <shrivastavaparth03@gmail.com>
2025-03-29 13:44:51 +0530

    Editing main.jsx and index.html

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,10 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import UserContext from "./context/UserContext.jsx";
 import "./index.css";
-import "./styles.css";
 import App from "./App.jsx";
 import DriverContext from "./context/DriverContext.jsx";
 import SocketProvider from "./context/SocketContext.jsx";
+import "./styles.css";
 
 createRoot(document.getElementById("root")).render(
   <DriverContext>
