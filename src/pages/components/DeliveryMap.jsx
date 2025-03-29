a28d39177ba4145f16b85f8cb20149ca250c8536
Parth Shrivastava <shrivastavaparth03@gmail.com>
2025-03-29 14:49:57 +0530

    Editing leaflet

--- src/pages/components/DeliveryMap.jsx
+++ src/pages/components/DeliveryMap.jsx
@@ -8,12 +8,9 @@ import { UserDataContext } from "../../context/UserContext";
 // Fix Leaflet marker icons
 delete L.Icon.Default.prototype._getIconUrl;
 L.Icon.Default.mergeOptions({
-  iconRetinaUrl:
-    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png",
-  iconUrl:
-    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png",
-  shadowUrl:
-    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png",
+  iconRetinaUrl: "/images/marker-icon-2x.png",
+  iconUrl: "/images/marker-icon.png",
+  shadowUrl: "/images/marker-shadow.png",
 });
 
 const DeliveryMap = ({
--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,32 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 export default defineConfig({
-  plugins: [react()], // Remove the tailwindcss() plugin
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: "node_modules/leaflet/dist/images/*",
+          dest: "images",
+        },
+      ],
+    }),
+  ],
   base: "/",
   build: {
     assetsDir: "assets",
     manifest: true,
     rollupOptions: {
       input: "./index.html",
+      output: {
+        manualChunks: {
+          leaflet: ["leaflet"],
+          react: ["react", "react-dom"],
+          vendor: ["lodash", "axios"],
+        },
+      },
     },
   },
   server: {
